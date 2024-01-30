3f691c80d957ce9dec21ebe5e79d3124d7e6493e
Sean O. Stalley <sstalley@pdx.edu>
2024-01-29 21:04:39 -0800

    Initial version from Jan 25th Lecture
    Have a charecter who moves around in a semi-realistic 1-d environment

---
+++ quest_adventure.py
@@ -0,0 +1,54 @@
+
+game_name = "Quest Adventure"
+print(f"Welcome to {game_name}")
+#name = input("Enter Player Name: ")
+name = "Snean"
+print(f"Welcome {name} to {game_name}!")
+
+#The Locations in our Adventure
+locations = [
+"Mills End Park",
+"Hwy 26 Tunnel",
+"Hillboro Stadium",
+"The 7 layers of the gumdrop forest",
+"Seaside",
+"Hawaii",
+"Taiwan",
+"Mongolia"]
+
+player_location = 0
+
+answer = ""
+
+print("The locations are:")
+for location in locations:
+    pass
+
+while player_location < len(locations)-1: #"l" not in answer.lower() and "r" not in answer.lower():
+
+    current_location = locations[player_location]
+    answer = input(f"You are at {current_location}. which way would you like to go (L/R)?")
+    print(f"you entered {answer}")
+
+    if "l" in answer.lower():
+        print("you want to go left")
+        #player_location = player_location + 1
+        player_location += 1
+    elif "r" in answer.lower():
+        print("you want to go right")
+        player_location -= 1
+    else:
+        print("Not Left or Right so i'm confused like MUSC")
+
+    if player_location < 0:
+        print("nice try :P")
+        player_location = 0
+
+print("YOU WIN")
+
+
+
+
+#if yes then go to show and have a good time
+
+#if not then be bored
